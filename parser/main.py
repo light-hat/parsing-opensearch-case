d0fdb6ece1bb46a45e821657be3778596021d7d7
Alexey <pirogov30002@gmail.com>
2025-08-01 04:20:18 +0300

    update dockerfile & script

---
+++ parser/main.py
@@ -0,0 +1,26 @@
+import time
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-webrtc")
+chrome_options.add_argument("--hide-scrollbars")
+chrome_options.add_argument("--disable-notifications")
+chrome_options.add_argument("--start-maximized")
+
+SELENIUM_REMOTE_URL = os.getenv("SELENIUM_REMOTE_URL", "http://selenium:4444/wd/hub")
+
+driver = webdriver.Remote(
+    command_executor=SELENIUM_REMOTE_URL,
+    options=chrome_options
+)
+
+driver.get("https://www.timeweb.cloud")
+
+driver.quit()
